9e9e0a9ca921669d4a269f0a628615ea0d611dd9
MengMaStar <MengMaStar@hotmail.com>
2024-09-05 23:45:38 +0800

    更新 pxt.json, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def on_on_chat():
+    pass
+player.on_chat("run", on_on_chat)
+
+agent.teleport_to_player()
--- main.ts
+++ main.ts
@@ -2,3 +2,6 @@ player.onChat("run", function () {
 	
 })
 agent.teleportToPlayer()
+for (let i = 0; i < 5; i++) {
+    
+}
